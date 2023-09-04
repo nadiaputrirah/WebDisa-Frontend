bbc2bd3bd465595e3eeddf9bdf80b2ec358a11b6
wibisana putra setyanegara <eizisana@gmail.com>
2023-09-04 11:08:15 +0700

    routing login / register

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import "./App.css";
 import Home from "./pages/Home/index";
+import Login from "./pages/Auth/login";
 import Info from "./pages/Information/info";
+import Populer from "./components/Populer/populer";
+import Register from "./pages/Auth/register";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/home");
+    }
+  }, [location, navigate]);
+
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/info" element={<Info />} />
-        </Routes>
-      </Router>
-    </div>
+    <Routes>
+      <Route path="/home/*" element={<Home />} />
+      <Route path="/info/*" element={<Info />} />
+      <Route path="/populer/*" element={<Populer />} />
+      <Route path="/login/*" element={<Login />} />
+      <Route path="/register/*" element={<Register />} />
+    </Routes>
   );
 }
 
