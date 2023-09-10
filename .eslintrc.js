f003f22bb964d3745a5427d254e5039b8b8bd858
Fadhil1 <muhfadhilrachman@gmail.com>
2023-09-10 14:05:16 +0700

    fixing eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
+    "react/jsx-key": "off",
+    "react-hooks/rules-of-hooks": "off",
+    "react/display-name": "off",
+    "no-unused-vars": "off",
+    "react/no-unknown-property": "off",
+    "no-undef": "off",
+    "react/prop-type": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+};
